314994e78245aa02e3c2967121d0dcb92a318535
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2019-03-17 10:16:08 -0600

    Using interpolation to submit multiple jobs.

--- kubernetes/run_jobs.sh
+++ kubernetes/run_jobs.sh
@@ -6,31 +6,29 @@ set -e
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # arguments - change as needed
-export DATA_DIRECTORY=/tmp
+export HOST_DIRECTORY=/tmp
 export PARAMETER_FILE=parameters/quickTest.xml
 export TREES_PER_DECADE=2
 export STRIDE=2
 
-# start $STRIDE containers
+# start $STRIDE jobs
 for ((i=0;i<=$STRIDE-1;i++)); 
 do
    	export OFFSET=$i
    
    	# create Kubernetes config map storing parameters for the job
    	echo "Executing iteration for stride=$STRIDE offset=$i"
-   	kubectl create configmap galacticus-config\
-   			--from-literal=DATA_DIRECTORY=${DATA_DIRECTORY}\
-   			--from-literal=PARAMETER_FILE=${PARAMETER_FILE}\
-   			--from-literal=TREES_PER_DECADE=${TREES_PER_DECADE}\
-   			--from-literal=STRIDE=${STRIDE}\
-   			--from-literal=OFFSET=${OFFSET}
-	
-	# run Kubernetes job for this configiration
-	kubectl create -f ${THIS_DIR}/galacticus.yml
-	
-	# delete this configuration
-	sleep 2
-	kubectl delete configmap galacticus-config
+   	
+   	# interpolate the job manifest in place
+   	# and use it to create a Kubernetes job
+    # note the use of the '#' character instead of '/' in the first two replacements
+   	cat ${THIS_DIR}/galacticus.yml | sed "s#@HOST_DIRECTORY@#${HOST_DIRECTORY}#"\
+   	                               | sed "s#@PARAMETER_FILE@#${PARAMETER_FILE}#"\
+   	                               | sed "s/@TREES_PER_DECADE@/${TREES_PER_DECADE}/"\
+   	                               | sed "s/@STRIDE@/${STRIDE}/"\
+   	                               | sed "s/@OFFSET@/${OFFSET}/"\
+   	                               | kubectl create -f -
+   		
       
 done
 
