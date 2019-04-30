1b26f55515489c377b9fdbd00764ce7ba24975fb
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2019-04-30 13:30:53 -0600

    Updating submission script.

--- ecs/run_ecs_tasks.py
+++ ecs/run_ecs_tasks.py
@@ -1,10 +1,44 @@
 # Python script to submit and monitor ECS tasks
 import boto3
+from time import sleep
+
+MAX_CONCURRENT_TASKS = 1
+NUM_TASKS = 10
+SLEEP_SECONDS = 1
 
 session = boto3.Session(profile_name='saml')
 client = session.client('ecs')
 
-response = client.list_tasks(cluster='EDRN', 
-                             family='galacticus', 
-                             desiredStatus='RUNNING')
-print("Number of runnig tasks:%s" % len(response["taskArns"]))
+itask = 0
+
+while itask < NUM_TASKS:
+
+    response = client.list_tasks(cluster='EDRN', 
+                                 family='galacticus', 
+                                 desiredStatus='RUNNING')
+    num_running_tasks = len(response["taskArns"])
+    print("Number of running tasks:%s" % num_running_tasks)
+    
+    if num_running_tasks < MAX_CONCURRENT_TASKS:
+        itask += 1
+        print("Submitting task # %s" % itask)
+        resp = client.run_task(cluster='EDRN', 
+                            taskDefinition="galacticus",
+                            launchType='EC2', 
+                            overrides={
+                                'containerOverrides': [
+                                    {
+                                    "name": "galacticus",
+                                    "environment": [
+                                        {
+                                            "name": "PARAMETER_FILE",
+                                            "value": "parameters/quickTest.xml"
+                                        }]
+                                    }
+                                ]
+                            }
+        )
+        print("Task submission response=%s" % resp)
+    else:
+        sleep(SLEEP_SECONDS)
+    
